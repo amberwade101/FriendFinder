d4f89f97ed7bb485b36f2f6d06557824250f546d
amber wade <amberwade@ambers-MacBook-Pro.local>
2018-06-21 21:13:30 -0700

    files added

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+var friendsArray=
+
+[
+	{
+		name: "Ahmed",
+		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+		scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+		]
+	},
+	{
+		name: "Louise Belcher",
+		photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b9/Louise_render.png/revision/latest?cb=20130113075349",
+		scores: [
+			2,
+			4,
+			1,
+			1,
+			1,
+			1,
+			1,
+			2,
+			1,
+			5
+		]
+	},
+	{
+		name: "Bob Belcher",
+		photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/3/3e/Bobbelcher2.png/revision/latest?cb=20130114075133",
+		scores: [
+			4,
+			3,
+			5,
+			1,
+			1,
+			5,
+			5,
+			1,
+			1,
+			1
+		]
+	},
+	{
+		"name": "Tina Belcher",
+		"photo": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/6/6e/Tina_render.png/revision/latest?cb=20130113192209",
+		"scores": [
+			5,
+			1,
+			2,
+			1,
+			3,
+			4,
+			1,
+			3,
+			3,
+			4
+		]
+	},
+	{
+		"name": "Linda Belcher",
+		"photo": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Lindab.png/revision/latest?cb=20130113071354",
+		"scores": [
+			1,
+			1,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			1
+		]
+	},
+	{
+		name: "Gene Belcher",
+		photo: "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Gene_Belcher.png/revision/latest?cb=20130114043308",
+		scores: [
+			1,
+			1,
+			5,
+			1,
+			3,
+			2,
+			5,
+			1,
+			1,
+			5
+		]
+	},
+	
+];
+
+
+
+
+
+
+  module.exports = friendsArray;
